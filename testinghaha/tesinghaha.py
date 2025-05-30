8d92b96aac6b1bf06230ed25774dc1fe7e79b84b
sem22-dev <thotjj22@gmail.com>
2025-05-30 13:25:37 +0530

    update

--- testinghaha/tesinghaha.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        my_set = set()
-        for num in nums:
-            if num in my_set:
-                return True
-            my_set.add(num)
